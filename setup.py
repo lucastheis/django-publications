c039e317c337615dfb6592af667a6a9594ff5a50
Alexandre Leray <alexandre@alexandreleray.com>
2012-06-04 16:22:45 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+
+from setuptools import setup
+from publications import __version__
+
+
+setup(
+    name='Django Publications',
+    version=__version__,
+    author='Lucas Theis',
+    author_email='lucas@theis.io',
+    description='A Django app for managing publications.',
+    url='https://github.com/lucastheis/django-publications',
+    packages=['publications'],
+    install_requires=['Python>=2.5.0', 'Django>=1.3.0'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
