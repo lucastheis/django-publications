d42cd11d2e222968ff6227331ef9330e53b8d09e
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2016-12-23 13:13:28 +0100

    WIP Updates GUI * Based on Bootstrap v4.0.0-alpha.5 * Refactor file inclusions to enforce homogeneity in views * Integrates in project's base layout (no more stand-alone)

--- publications/templatetags/publication_extras.py
+++ publications/templatetags/publication_extras.py
@@ -2,109 +2,112 @@ __license__ = 'MIT License <http://www.opensource.org/licenses/mit-license.php>'
 __author__ = 'Lucas Theis <lucas@theis.io>'
 __docformat__ = 'epytext'
 
-import os
-import django
-
 from distutils.version import StrictVersion
-from django.template import Library, Node, Context, RequestContext
+from re import sub
+
+import django
+from django.template import Library, RequestContext
 from django.template.loader import get_template
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
+
 from publications.models import Publication, List, Type
 from publications.utils import populate
-from re import sub
 
 register = Library()
 
 GREEK_LETTERS = \
-	'[Aa]lpha|[Bb]eta|[Gg]amma|[Dd]elta|[Ee]psilon|[Zz]eta|' + \
-	'[Ee]ta|[Tt]heta|[Ll]ambda|[Mm]u|[Nn]u|[Pp]i|[Ss]igma|[Tt]au|' + \
-	'[Pp]hi|[Pp]si|[Cc]hi|[Oo]mega|[Rr]ho|[Xx]i|[Kk]appa'
+    '[Aa]lpha|[Bb]eta|[Gg]amma|[Dd]elta|[Ee]psilon|[Zz]eta|' + \
+    '[Ee]ta|[Tt]heta|[Ll]ambda|[Mm]u|[Nn]u|[Pp]i|[Ss]igma|[Tt]au|' + \
+    '[Pp]hi|[Pp]si|[Cc]hi|[Oo]mega|[Rr]ho|[Xx]i|[Kk]appa'
 
 
 def render_template(template, request, args):
-	if StrictVersion(django.get_version()) < StrictVersion('1.8.0'):
-		return get_template(template).render(RequestContext(request, args))
-	return get_template(template).render(args, request)
+    if StrictVersion(django.get_version()) < StrictVersion('1.8.0'):
+        return get_template(template).render(RequestContext(request, args))
+    return get_template(template).render(args, request)
 
 
-def get_publications(context, template='publications/publications.html'):
-	"""
-	Get all publications.
-	"""
+@register.simple_tag(takes_context=True)
+def get_publications(context, template='publications/pages/publications.html'):
+    """
+    Get all publications.
+    """
 
-	types = Type.objects.filter(hidden=False)
-	publications = Publication.objects.select_related()
-	publications = publications.filter(external=False, type__in=types)
-	publications = publications.order_by('-year', '-month', '-id')
+    types = Type.objects.filter(hidden=False)
+    publications = Publication.objects.select_related()
+    publications = publications.filter(external=False, type__in=types)
+    publications = publications.order_by('-year', '-month', '-id')
 
-	if not publications:
-		return ''
+    if not publications:
+        return ''
 
-	# load custom links and files
-	populate(publications)
+    # load custom links and files
+    populate(publications)
 
-	return render_template(template, context['request'], {'publications': publications})
+    return render_template(template, context['request'], {'publications': publications})
 
 
+@register.simple_tag(takes_context=True)
 def get_publication(context, id):
-	"""
-	Get a single publication.
-	"""
+    """
+    Get a single publication.
+    """
 
-	pbl = Publication.objects.filter(pk=int(id))
+    pbl = Publication.objects.filter(pk=int(id))
 
-	if len(pbl) < 1:
-		return ''
+    if len(pbl) < 1:
+        return ''
 
-	pbl[0].links = pbl[0].customlink_set.all()
-	pbl[0].files = pbl[0].customfile_set.all()
+    pbl[0].links = pbl[0].customlink_set.all()
+    pbl[0].files = pbl[0].customfile_set.all()
 
-	return render_template(
-		'publications/publication.html', context['request'], {'publication': pbl[0]})
-
-
-def get_publication_list(context, list, template='publications/publications.html'):
-	"""
-	Get a publication list.
-	"""
-
-	list = List.objects.filter(list__iexact=list)
-
-	if not list:
-		return ''
-
-	list = list[0]
-	publications = list.publication_set.all()
-	publications = publications.order_by('-year', '-month', '-id')
-
-	if not publications:
-		return ''
-
-	# load custom links and files
-	populate(publications)
-
-	return render_template(
-		template, context['request'], {'list': list, 'publications': publications})
+    return render_template('publications/components/publication.html', context['request'], {'publication': pbl[0]})
 
 
+@register.simple_tag(takes_context=True)
+def get_publication_list(context, list, template='publications/pages/publications.html'):
+    """
+    Get a publication list.
+    """
+
+    list = List.objects.filter(list__iexact=list)
+
+    if not list:
+        return ''
+
+    list = list[0]
+    publications = list.publication_set.all()
+    publications = publications.order_by('-year', '-month', '-id')
+
+    if not publications:
+        return ''
+
+    # load custom links and files
+    populate(publications)
+
+    return render_template(template, context['request'], {'list': list, 'publications': publications})
+
+
+@register.filter()
 def tex_parse(string):
-	"""
-	Renders some basic TeX math to HTML.
-	"""
+    """
+    Renders some basic TeX math to HTML.
+    """
 
-	string = string.replace('{', '').replace('}', '')
-	def tex_replace(match):
-		return \
-			sub(r'\^(\w)', r'<sup>\1</sup>',
-			sub(r'\^\{(.*?)\}', r'<sup>\1</sup>',
-			sub(r'\_(\w)', r'<sub>\1</sub>',
-			sub(r'\_\{(.*?)\}', r'<sub>\1</sub>',
-			sub(r'\\(' + GREEK_LETTERS + ')', r'&\1;', match.group(1))))))
-	return mark_safe(sub(r'\$([^\$]*)\$', tex_replace, escape(string)))
+    string = string.replace('{', '').replace('}', '')
+
+    def tex_replace(match):
+        return \
+            sub(r'\^(\w)', r'<sup>\1</sup>',
+                sub(r'\^\{(.*?)\}', r'<sup>\1</sup>',
+                    sub(r'\_(\w)', r'<sub>\1</sub>',
+                        sub(r'\_\{(.*?)\}', r'<sub>\1</sub>',
+                            sub(r'\\(' + GREEK_LETTERS + ')', r'&\1;', match.group(1))))))
+
+    return mark_safe(sub(r'\$([^\$]*)\$', tex_replace, escape(string)))
 
 
-register.simple_tag(get_publications, takes_context=True)
-register.simple_tag(get_publication, takes_context=True)
-register.simple_tag(get_publication_list, takes_context=True)
-register.filter('tex_parse', tex_parse)
+@register.filter(is_safe=False)
+def as_list(o):
+    return [o]
--- publications/views/list.py
+++ publications/views/list.py
@@ -4,49 +4,52 @@ __docformat__ = 'epytext'
 
 from django.http import Http404
 from django.shortcuts import render
-from publications.models import List, Type, Publication, CustomLink, CustomFile
+
+from publications.models import List
 from publications.utils import populate
 
+
 def list(request, list):
-	list = List.objects.filter(list__iexact=list)
+    list = List.objects.filter(list__iexact=list)
 
-	if not list:
-		raise Http404
+    if not list:
+        raise Http404
 
-	list = list[0]
-	publications = list.publication_set.all()
-	publications = publications.order_by('-year', '-month', '-id')
+    list = list[0]
+    publications = list.publication_set.all()
+    publications = publications.order_by('-year', '-month', '-id')
 
-	if 'plain' in request.GET:
-		return render(request, 'publications/publications.txt', {
-				'publications': publications
-			}, content_type='text/plain; charset=UTF-8')
+    if 'plain' in request.GET:
+        return render(request, 'publications/export/publications.txt', {
+            'publications': publications
+        }, content_type='text/plain; charset=UTF-8')
 
-	if 'bibtex' in request.GET:
-		return render(request, 'publications/publications.bib', {
-				'publications': publications
-			}, content_type='text/x-bibtex; charset=UTF-8')
+    if 'bibtex' in request.GET:
+        return render(request, 'publications/export/publications.bib', {
+            'publications': publications
+        }, content_type='text/x-bibtex; charset=UTF-8')
 
-	if 'mods' in request.GET:
-		return render(request, 'publications/publications.mods', {
-				'publications': publications
-			}, content_type='application/xml; charset=UTF-8')
+    if 'mods' in request.GET:
+        return render(request, 'publications/export/publications.mods', {
+            'publications': publications
+        }, content_type='application/xml; charset=UTF-8')
 
-	if 'ris' in request.GET:
-		return render(request, 'publications/publications.ris', {
-				'publications': publications
-			}, content_type='application/x-research-info-systems; charset=UTF-8')
+    if 'ris' in request.GET:
+        return render(request, 'publications/export/publications.ris', {
+            'publications': publications
+        }, content_type='application/x-research-info-systems; charset=UTF-8')
 
-	if 'rss' in request.GET:
-		return render(request, 'publications/publications.rss', {
-				'url': 'http://' + request.get_host() + request.path,
-				'publications': publications
-			}, content_type='application/rss+xml; charset=UTF-8')
+    if 'rss' in request.GET:
+        return render(request, 'publications/export/publications.rss', {
+            'url': 'http://' + request.get_host() + request.path,
+            'publications': publications
+        }, content_type='application/rss+xml; charset=UTF-8')
 
-	# load custom links and files
-	populate(publications)
+    # load custom links and files
+    populate(publications)
 
-	return render(request, 'publications/list.html', {
-			'list': list,
-			'publications': publications
-		})
+    return render(request, 'publications/pages/list.html', {
+        'list': list,
+        'publications': publications,
+        'title': "publications for list {}".format(list)
+    })
