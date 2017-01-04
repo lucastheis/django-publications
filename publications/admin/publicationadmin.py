b712e7ccaccbd3fbbdd3c60cb7b723158a807f05
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-01-04 15:34:07 +0100

    Update admin fields order and sets

--- publications/admin/publicationadmin.py
+++ publications/admin/publicationadmin.py
@@ -3,49 +3,58 @@ __author__ = 'Lucas Theis <lucas@theis.io>'
 __docformat__ = 'epytext'
 
 from django.contrib import admin
+
 try:
-	from django.conf.urls import url
+    from django.conf.urls import url
 except ImportError:
-	from django.conf.urls.defaults import url
+    from django.conf.urls.defaults import url
 from publications.models import CustomLink, CustomFile
 
 import publications.admin_views
 
+
 class CustomLinkInline(admin.StackedInline):
-	model = CustomLink
-	extra = 1
-	max_num = 5
+    model = CustomLink
+    extra = 1
+    max_num = 5
 
 
 class CustomFileInline(admin.StackedInline):
-	model = CustomFile
-	extra = 1
-	max_num = 5
+    model = CustomFile
+    extra = 1
+    max_num = 5
 
 
 class PublicationAdmin(admin.ModelAdmin):
-	list_display = ('type', 'first_author', 'title', 'type', 'year', 'journal_or_book_title')
-	list_display_links = ('title',)
-	change_list_template = 'admin/publications/publication_change_list.html'
-	search_fields = ('title', 'journal', 'authors', 'keywords', 'year')
-	fieldsets = (
-		(None, {'fields':
-			('type', 'title', 'authors', 'year', 'month')}),
-		(None, {'fields':
-			('journal', 'book_title', 'publisher', 'institution', 'volume', 'number', 'pages')}),
-		(None, {'fields':
-			('citekey', 'keywords', 'url', 'code', 'pdf', 'doi', 'isbn', 'note', 'external')}),
-		(None, {'fields':
-			('abstract',)}),
-		(None, {'fields':
-			('image', 'thumbnail')}),
-		(None, {'fields':
-			('lists',)}),
-	)
-	inlines = [CustomLinkInline, CustomFileInline]
+    list_display = ('type', 'first_author', 'title', 'type', 'year', 'journal_or_book_title')
+    list_display_links = ('title',)
+    change_list_template = 'admin/publications/publication_change_list.html'
+    search_fields = ('title', 'journal', 'authors', 'keywords', 'year')
+    fieldsets = (
+        (None,
+         {'fields':
+              ('type', 'title', 'authors', 'year', 'month', 'external',)}),
+        ('Publishing',
+         {'fields':
+              ('journal', 'book_title', 'publisher', 'institution', 'volume', 'number', 'pages',
+               'url', 'doi', 'isbn',)}),
+        ('References',
+         {'fields':
+              ('citekey', 'keywords', 'code',)}),
+        (None,
+         {'fields':
+              ('abstract', 'note',)}),
+        ('Media',
+         {'classes': ('collapse',), 'fields':
+             ('pdf', 'image', 'thumbnail')}),
+        (None,
+         {'fields':
+              ('lists',)}),
+    )
+    inlines = [CustomLinkInline, CustomFileInline]
 
-	def get_urls(self):
-		return [
-				url(r'^import_bibtex/$', publications.admin_views.import_bibtex,
-					name='publications_publication_import_bibtex'),
-			] + super(PublicationAdmin, self).get_urls()
+    def get_urls(self):
+        return [
+                   url(r'^import_bibtex/$', publications.admin_views.import_bibtex,
+                       name='publications_publication_import_bibtex'),
+               ] + super(PublicationAdmin, self).get_urls()
