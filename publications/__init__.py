23030b30df34da9e2e531557f349c9ee969a1974
Marc Bourqui <marc@bourqui.org>
2017-01-05 09:22:03 +0100

    Add more accented characters for bibtex parser

--- publications/__init__.py
+++ publications/__init__.py
@@ -2,6 +2,6 @@ __license__ = 'MIT License <http://www.opensource.org/licenses/mit-license.php>'
 __authors__ = ['Lucas Theis <lucas@theis.io>', 'Marc Bourqui']
 __credits__ = ['Lucas Theis', 'Marc Bourqui', 'Maarten van Gompel']
 __docformat__ = 'epytext'
-__version__ = '0.8.1-dev'
+__version__ = '0.8.3-dev'
 __date__ = '04.01.2017'
 __status__ = 'Development'
