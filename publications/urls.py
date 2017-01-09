58f391daf54d01bb66f8eaa262e12e9fb87dec22
Marc Bourqui <marc@bourqui.org>
2017-01-09 11:28:11 +0100

    URLs use a dedicated namespace. Inspired by:  * f6e6282 from @proycon  * 2a55213 from @proycon

--- publications/urls.py
+++ publications/urls.py
@@ -3,18 +3,19 @@ __author__ = 'Lucas Theis <lucas@theis.io>'
 __docformat__ = 'epytext'
 
 try:
-	from django.conf.urls import url
+    from django.conf.urls import url
 except ImportError:
-	from django.conf.urls.defaults import url
+    from django.conf.urls.defaults import url
 
 from publications import views
 
+app_name = 'publications'
 urlpatterns = [
-	url(r'^$', views.year, name='publications.views.year'),
-	url(r'^(?P<publication_id>\d+)/$', views.id, name='publications.views.id'),
-	url(r'^year/(?P<year>\d+)/$', views.year, name='publications.views.year'),
-	url(r'^tag/(?P<keyword>.+)/$', views.keyword, name='publications.views.keyword'),
-	url(r'^list/(?P<list>.+)/$', views.list, name='publications.views.list'),
-	url(r'^unapi/$', views.unapi, name='publications.views.unapi'),
-	url(r'^(?P<name>.+)/$', views.person, name='publications.views.person'),
+    url(r'^$', views.year, name='index'),
+    url(r'^(?P<publication_id>\d+)/$', views.id, name='id'),
+    url(r'^year/(?P<year>\d+)/$', views.year, name='year'),
+    url(r'^tag/(?P<keyword>.+)/$', views.keyword, name='keyword'),
+    url(r'^list/(?P<list>.+)/$', views.list, name='list'),
+    url(r'^unapi/$', views.unapi, name='unapi'),
+    url(r'^(?P<name>.+)/$', views.author, name='author'),
 ]
