166913dd2b9220f0eff71a83d3523f42788018c7
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-04-07 10:53:18 +0200

    Close #15

---
+++ publications_bootstrap/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class PublicationsBootstrapConfig(AppConfig):
+    name = 'publications_bootstrap'
+    verbose_name = "Bootstrap-powered scientific publications for Django"
+
+    # TODO: check if dependencies are met
