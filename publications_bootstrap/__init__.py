8b22d4f4d54f744e9e51bed76a943a11e5dd0ba2
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-05-30 09:38:46 +0200

    Change docformat to numpy

--- publications_bootstrap/__init__.py
+++ publications_bootstrap/__init__.py
@@ -3,7 +3,7 @@
 
 __license__ = 'MIT License <http://www.opensource.org/licenses/mit-license.php>'
 __authors__ = ['Lucas Theis <lucas@theis.io>', 'Marc Bourqui <https://github.com/mbourqui>']
-__docformat__ = 'epytext'
+__docformat__ = 'numpy'
 __version__ = '2.0.3'
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
 __status__ = 'Stable'
