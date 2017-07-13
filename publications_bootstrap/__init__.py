6b649080d8582ce5fed5740bafab8f34e567ab2f
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-07-13 17:31:55 +0200

    Update contributor email

--- publications_bootstrap/__init__.py
+++ publications_bootstrap/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __license__ = 'MIT License <http://www.opensource.org/licenses/mit-license.php>'
-__authors__ = ['Lucas Theis <lucas@theis.io>', 'Marc Bourqui <https://github.com/mbourqui>']
+__authors__ = ['Lucas Theis <lucas@theis.io>', 'Marc Bourqui <pypi.kemar@bourqui.org>']
 __docformat__ = 'numpy'
 __version__ = '2.1.0'
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
