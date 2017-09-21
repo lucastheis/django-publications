1e1966beb901299ac175a758d45ee4b832d07913
Marc Bourqui <marc@bourqui.org>
2017-09-21 23:18:21 +0200

    Bump to v2.2.1

--- publications_bootstrap/__init__.py
+++ publications_bootstrap/__init__.py
@@ -4,6 +4,6 @@
 __license__ = 'MIT License <http://www.opensource.org/licenses/mit-license.php>'
 __authors__ = ['Lucas Theis <lucas@theis.io>', 'Marc Bourqui <pypi.kemar@bourqui.org>']
 __docformat__ = 'numpy'
-__version__ = '2.2.0'
+__version__ = '2.2.1'
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
 __status__ = 'Stable'
