c81aa2259ee97468773785c21446d72df86eac01
Lucas Theis <lucas@theis.io>
2019-10-04 12:59:45 +0100

    Bump version to 1.0.0

--- publications/__init__.py
+++ publications/__init__.py
@@ -1,4 +1,4 @@
 __license__ = 'MIT License <http://www.opensource.org/licenses/mit-license.php>'
 __author__ = 'Lucas Theis <lucas@theis.io>'
 __docformat__ = 'epytext'
-__version__ = '0.6.2dev'
+__version__ = '1.0.0'
